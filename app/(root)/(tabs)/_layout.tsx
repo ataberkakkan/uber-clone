940fa05020f037925e445179b45cb9de118fe335
ataberkakkan <akkanataberk@gmail.com>
2024-08-21 09:39:11 +0300

    Ride Card

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -10,17 +10,15 @@ const TabIcon = ({
   focused: boolean;
 }) => {
   return (
-    <View className="rounded-full">
-      <View
-        className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
-      >
-        <Image
-          source={source}
-          tintColor="white"
-          resizeMode="contain"
-          className="w-7 h-7"
-        />
-      </View>
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+    >
+      <Image
+        source={source}
+        tintColor="white"
+        resizeMode="contain"
+        className="w-7 h-7"
+      />
     </View>
   );
 };
