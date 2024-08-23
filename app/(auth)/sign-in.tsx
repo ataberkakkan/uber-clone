12f5635eca3ad84822df6454accf0a1cab357ab2
ataberkakkan <akkanataberk@gmail.com>
2024-08-23 18:33:45 +0300

    Fixed redirected route after signing in

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -30,7 +30,7 @@ const SignIn = () => {
 
       if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
+        router.replace("/(root)/(tabs)/home");
       } else {
         // See https://clerk.com/docs/custom-flows/error-handling
         // for more info on error handling
